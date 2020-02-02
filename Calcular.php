33cc96ac2522a224bca7667eccaee0e4c0920bbf
DarkJ-18 <54506019+DarkJ-18@users.noreply.github.com>
2020-02-02 14:00:15 -0500

    Add files via upload

---
+++ Calcular.php
@@ -0,0 +1,55 @@
+<?php 
+    if (isset($_POST["btncalcular"])) {
+        sleep(2);
+     
+      
+        require_once 'logica/Salario.php';
+        require_once 'logica/Salud.php';
+        require_once 'logica/Pension.php';
+        require_once 'logica/Caja.php';
+
+        $cantidadhoras = $_POST["txthorastrabajadas"];
+        $valorhora = $_POST["txtcostohoratrabajo"];
+      
+        $objSalario = new Salario($cantidadhoras,$valorhora);
+        $objSalud = new Salud($cantidadhoras,$valorhora);
+        $objPension = new Pension($cantidadhoras,$valorhora);
+        $objCaja = new Caja($cantidadhoras,$valorhora);
+
+        echo "<b>El salario bruto es: </b>" . $objSalario->calcularSalarioBruto() . "<br>";
+
+        if($objSalario->calcularSalarioBruto() <= 1000000){
+          
+            echo "<b> El descuento de salud es: </b>" . $objSalud->calcularSalud(0.02) . "<br>";
+            echo "<b> El descuento de pension es: </b>" . $objPension->calcularPension(0.04) . "<br>";
+
+            $incremento = $objSalario->calcularSalarioBruto() * 0.03;
+            echo "<b> Incremento: </b>" . $incremento . "<br>";
+
+            $salarioFinal = ($objSalario->calcularSalarioBruto() + $incremento) - $objSalud->calcularSalud(0.02) - $objPension->calcularPension(0.04);
+            echo "<b>Salario a pagar: </b>" . $salarioFinal . "<br>";
+        }
+        else if($objSalario->calcularSalarioBruto() > 1000000 && $objSalario->calcularSalarioBruto() <=2000000) {
+         
+            echo "<b> El descuento de salud es: </b>" . $objSalud->calcularSalud(0.04) . "<br>";
+            echo "<b> El descuento de pension es: </b>" . $objPension->calcularPension(0.06) . "<br>";
+            echo "<b> El descuento de cada caja de compensaciones es: </b>" . $objCaja->calcularCaja(0.02) . "</br>";
+
+            $salarioFinal = $objSalario->calcularSalarioBruto() - $objSalud->calcularSalud(0.04) - $objPension->calcularPension(0.06) - $objCaja->calcularCaja(0.02);
+            echo "<b>Salario a pagar : </b>" . $salarioFinal . "</br>";
+        }
+        else if($objSalario->calcularSalarioBruto() > 2000000) {
+          
+            echo "<b> El descuento de salud es: </b>" . $objSalud->calcularSalud(0.05) . "<br>";
+            echo "<b> El descuento de pension es: </b>" . $objPension->calcularPension(0.07) . "<br>";
+            echo "<b> El descuento de cada caja de compensaciones es: </b>" . $objCaja->calcularCaja(0.03) . "</br>";
+
+            $salarioFinal = $objSalario->calcularSalarioBruto() - $objSalud->calcularSalud(0.05) - $objPension->calcularPension(0.07) - $objCaja->calcularCaja(0.03);
+            echo "<b>Salario a pagar : </b>" . $salarioFinal . "</br>";
+        }
+        
+    }
+    else {
+        echo "El FBI va en camino, CORRE PERRA, CORRE!!";
+    }
+?>
